855a71f01fb24574f9dd00130638cf5f01355093
Lviwer <szkudlarek91@gmail.com>
2019-07-17 19:46:55 +0200

    Switch project to maven project

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import entity.Boots;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Boots gymShoes = new Boots(1, "Adidas", 54.99f, 1.2f, "Red",
+                1, 38,false);
+        System.out.println(gymShoes.toString());
+
+
+
+
+    }
+}
